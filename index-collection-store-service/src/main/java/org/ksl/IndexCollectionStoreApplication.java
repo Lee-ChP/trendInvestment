df742aafc7a7a2367f9922daee08fcfe63482305
keelahselai <leejanezeroone10@gmail.com>
2021-05-07 17:23:41 +0800

    数据采集服务:添加redis缓存

--- index-collection-store-service/src/main/java/org/ksl/IndexCollectionStoreApplication.java
+++ index-collection-store-service/src/main/java/org/ksl/IndexCollectionStoreApplication.java
@@ -7,7 +7,9 @@ import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,12 +20,15 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableCaching
 @Slf4j
 public class IndexCollectionStoreApplication {
     public static void main(String[] args) {
         int port;
         int defaultPort = 8001;
         int eurekaServerPort = 8761;
+        int redisPort = 6379;
         port = defaultPort;
 
         if (NetUtil.isUsableLocalPort(eurekaServerPort)) {
@@ -31,19 +36,24 @@ public class IndexCollectionStoreApplication {
             System.exit(1);
         }
 
-        if (null != args && 0!=args.length) {
+        if(NetUtil.isUsableLocalPort(redisPort)) {
+            log.error("检查到端口{} 未启用，判断 redis 服务器没有启动，本服务无法使用，故退出!", redisPort );
+            System.exit(1);
+        }
+
+        if (null != args && 0 != args.length) {
             for (String arg : args) {
-                if(arg.startsWith("port=")) {
-                    String strPort= StrUtil.subAfter(arg, "port=", true);
-                    if(NumberUtil.isNumber(strPort)) {
+                if (arg.startsWith("port=")) {
+                    String strPort = StrUtil.subAfter(arg, "port=", true);
+                    if (NumberUtil.isNumber(strPort)) {
                         port = Convert.toInt(strPort);
                     }
                 }
             }
         }
 
-        if(!NetUtil.isUsableLocalPort(port)) {
-            log.error("端口{}被占用了，无法启动!", port );
+        if (!NetUtil.isUsableLocalPort(port)) {
+            log.error("端口{}被占用了，无法启动!", port);
             System.exit(1);
         }
         new SpringApplicationBuilder(IndexCollectionStoreApplication.class).properties("server.port=" + port).run(args);
--- index-collection-store-service/src/main/java/org/ksl/service/IndexService.java
+++ index-collection-store-service/src/main/java/org/ksl/service/IndexService.java
@@ -1,6 +1,12 @@
 package org.ksl.service;
 
+import cn.hutool.core.collection.CollectionUtil;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import lombok.extern.slf4j.Slf4j;
 import org.ksl.pojo.Index;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,9 +19,10 @@ import java.util.Map;
  * @author Keelahselai
  * @date 2021/05/07
  * description 指数服务组件
- *
  */
 @Service
+@Slf4j
+@CacheConfig(cacheNames = "indices")
 public class IndexService {
 
     @Resource
@@ -24,14 +31,28 @@ public class IndexService {
     /**
      * 从第三方获取指数数据
      * @return 转换后的实体列表
+     *
+     * HystrixProperty: 超时配置，否则默认为1s
      */
+    @HystrixCommand(fallbackMethod = "third_part_not_connected",
+            commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")})
+    @Cacheable(key = "'all_codes'")
     public List<Index> fetchIndexFromThirdPart() {
-        List<Map> temp = restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json",List.class);
+        List<Map> temp = restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json", List.class);
         return convertMapToIndex(temp);
+    }
+
+    public List<Index> third_part_not_connected() {
+        log.error("third_part_not_connected()");
+        Index index = new Index();
+        index.setCode("000000");
+        index.setName("无效指数代码");
+        return CollectionUtil.toList(index);
     }
 
     /**
      * 将第三方原始数据转成index实体
+     *
      * @param temp 第三方原始数据
      * @return 转换后的实体列表
      */
